6304314bf65d48c159290ed482931e517a7717e9
DiDiaz3110 <myzhanym@gmail.com>
2021-05-08 11:34:16 -0400

    by 11:35

--- src/day43_list_custom_classes/MethodsWithListReturn.java
+++ src/day43_list_custom_classes/MethodsWithListReturn.java
@@ -1,7 +1,27 @@
 package day43_list_custom_classes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodsWithListReturn {
     public static void main(String[] args) {
-        
+        List<Integer> mlnNums = getIntegerList();// returns ready ArrayList object. No need new
+        System.out.println("mlnNums = " + mlnNums);
     }
-}
+    /**
+     * getIntegerList
+     * No params
+     * return List<Integer>
+     *    Loop from 0 - 1000_000
+     *    and add to arraylist then return it
+     */
+        public static List<Integer> getIntegerList(){
+            List<Integer> nums = new ArrayList<>();//ArrayList<Integer> list = new ArrayList<>();
+            for(int i=0;i<=1_000_000;i++) {
+                nums.add(i);
+            }
+            return nums;
+            }
+        }
+
+
